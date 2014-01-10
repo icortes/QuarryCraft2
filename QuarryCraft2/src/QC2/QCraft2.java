c23cf98eb1f96dcb7cab3cdcd947150aefbc7f70
pstorum <p-storum@hotmail.com>
2014-01-10 10:38:04 -0800

    Main method2

--- QuarryCraft2/src/QC2/QCraft2.java
+++ QuarryCraft2/src/QC2/QCraft2.java
@@ -1,9 +1,17 @@
 package QC2;
 
+import QC2.QCraft2;
+import jgame.ImageCache;
+
 public class QCraft2 {
+	public enum View {
+		GAMEMAIN, GAME, OPTIONS, GAME_OVER;
 
+	}
 	public static void main(String[] args) {
-
+		ImageCache.create(QCraft2.class, "/QC2/rsc/");
+		QCraft2 craftyness = new QCraft2();
+		craftyness.startGame("QCraft2");
 	}
 
 	public QCraft2() {
