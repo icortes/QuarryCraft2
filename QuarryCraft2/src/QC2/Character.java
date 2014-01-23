963f8b6c4dd573aed230209132753c5638cbb3fc
pstorum <p-storum@hotmail.com>
2014-01-23 12:25:36 -0800

    platform controller ish

--- QuarryCraft2/src/QC2/Character.java
+++ QuarryCraft2/src/QC2/Character.java
@@ -8,8 +8,8 @@ import jgame.controller.KeyboardLocationController;
 public class Character extends GSprite{
 	public Character(){
 		super(ImageCache.forClass(QCraft2.class).get("qc2_soiltop.png"));
-		PlatformController klc = new PlatformController;
-		addPlatformController(klc);
+		//PlatformController klc = new PlatformController;
+		//addPlatformController(klc);
 		}
 
 }
